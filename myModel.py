963e38a0306a99f7ee3d21ec9231530c25517194
Harish S Kanthadai <79399996+Harish-S-Kanthadai-hari03k@users.noreply.github.com>
2023-09-23 04:19:13 +0530

    Update myModel.py

--- myModel.py
+++ myModel.py
@@ -3,33 +3,44 @@ import typing
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from your_project.inferenceModel import OnnxInferenceModel
-from your_project.utils.text_utils import ctc_decoder, get_cer
+from your_project.inference_model import OnnxInferenceModel
+from your_project.text_utils import ctc_decoder, get_cer
+from your_project.model_configs import HandwritingRecognitionModelConfigs
 
-class ImageToWordModel(OnnxInferenceModel):
-    def __init__(self, model_path: str, *args, **kwargs):
-        super().__init__(model_path, *args, **kwargs)
+class HandwritingRecognitionImageToWordModel(OnnxInferenceModel):
+    def __init__(self, model_path: str, char_list: typing.Union[str, list], *args, **kwargs):
+        super().__init__(model_path=model_path, *args, **kwargs)
+        self.char_list = char_list
 
     def predict(self, image: np.ndarray) -> str:
-        image = cv2.resize(image, self.input_shape[:2][::-1])
-        image_pred = np.expand_dims(image, axis=0).astype(np.float32)
+        resized_image = cv2.resize(image, self.input_shape[:2][::-1])
+        image_pred = np.expand_dims(resized_image, axis=0).astype(np.float32)
         preds = self.model.run(None, {self.input_name: image_pred})[0]
-        text = ctc_decoder(preds, self.vocab)[0]
+        text = ctc_decoder(preds, self.char_list)[0]
         return text
 
 if __name__ == "__main__":
-    model_path = "Models/08_handwriting_recognition_tf/202303142139/model.onnx"
-    val_csv_path = "Models/08_handwriting_recognition_tf/202303142139/val.csv"
+    configs = HandwritingRecognitionModelConfigs.load("Models/03_handwriting_recognition/202301111911/configs.yaml")
+    model_path = configs.model_path
+    char_list = configs.vocab
+    val_csv_path = "Models/03_handwriting_recognition/202301111911/val.csv"
 
-    model = ImageToWordModel(model_path=model_path)
+    model = HandwritingRecognitionImageToWordModel(model_path=model_path, char_list=char_list)
     df = pd.read_csv(val_csv_path).values.tolist()
 
-    accum_cer = []
+    cer_values = []
     for image_path, label in tqdm(df):
         image = cv2.imread(image_path)
         prediction_text = model.predict(image)
         cer = get_cer(prediction_text, label)
         print(f"Image: {image_path}, Label: {label}, Prediction: {prediction_text}, CER: {cer}")
-        accum_cer.append(cer)
+        cer_values.append(cer)
 
-    print(f"Average CER: {np.average(accum_cer)}")
+        # Resize the image by 4x for display
+        resized_image = cv2.resize(image, (image.shape[1] * 4, image.shape[0] * 4))
+        cv2.imshow("Image", resized_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    average_cer = np.average(cer_values)
+    print(f"Average CER: {average_cer}")
